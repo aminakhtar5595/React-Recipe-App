99d36f6b00b3ede5c2f742ab23cf44d5b041b175
Amin Akhtar <aminakhtar5595@gmail.com>
2019-11-30 12:56:52 +0500

    Updates

---
+++ precache-manifest.916ebded66339f22c1e27b1beebd31ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2fe0abbbcffa941688833a2fa8b0ca",
+    "url": "/React-Recipe-App/index.html"
+  },
+  {
+    "revision": "13e72a1482bb9ed7e299",
+    "url": "/React-Recipe-App/static/css/main.c496d7b3.chunk.css"
+  },
+  {
+    "revision": "2ba9adbb737d95f4002f",
+    "url": "/React-Recipe-App/static/js/2.4c839dd3.chunk.js"
+  },
+  {
+    "revision": "13e72a1482bb9ed7e299",
+    "url": "/React-Recipe-App/static/js/main.f499969d.chunk.js"
+  },
+  {
+    "revision": "03ae5b02dbb013e04365",
+    "url": "/React-Recipe-App/static/js/runtime-main.61caa50e.js"
+  }
+]);
